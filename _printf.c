b2d2668a5dc499ed62d9bcb86bb8166b108edfb5
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-05-07 22:34:59 +0000

    P

--- _printf.c
+++ _printf.c
@@ -9,6 +9,7 @@
 int _printf(const char *format, ...)
 {
 	int len, i, num_char = 0;
+	char c;
 	va_list args;
 
 	if (format == NULL)
@@ -20,21 +21,30 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	for (i = 0; format[i]; i++)
 	{
-	if (format[i] == '%' && check_mode(format[i + 1]))
-	{
-		check_func(args, format[i + 1], &num_char, &i);
-		check_func2(args, format[i + 1], &num_char, &i);
-	}
-	else if (format[i] == '%' && check_flag(format[i + 1]))
-	{
-		check_func_flag(args, format[i + 1], format[i + 2],
-				&num_char, &i);
-	}
-	else
-	{
-		write(1, &format[i], 1);
-		num_char++;
-	}
+		if (format[i] == '%' && check_mode(format[i + 1]))
+		{
+			check_func(args, format[i + 1], &num_char, &i);
+			check_func2(args, format[i + 1], &num_char, &i);
+		}
+		else if (format[i] == '%' && (check_flag(format[i + 1])
+					&& check_flag(format[i + 2])))
+		{
+			if (format[i + 1] == '+' || format[i + 2] == '+')
+				c = '+';
+			check_func_flag(args, c, format[i + 3],
+					&num_char, &i);
+			i++;
+		}
+		else if (format[i] == '%' && check_flag(format[i + 1]))
+		{
+			check_func_flag(args, format[i + 1], format[i + 2],
+					&num_char, &i);
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			num_char++;
+		}
 	}
 	va_end(args);
 	return (num_char);
